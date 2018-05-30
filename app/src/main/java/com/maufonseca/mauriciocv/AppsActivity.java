cb190e88ce3c02a52358b30f8c4cca8c2f37c568
Mauricio Fonseca <maubfon@gmail.com>
2018-05-29 23:27:47 -0300

    Add about app and developed apps activities entry points

--- app/src/main/java/com/maufonseca/mauriciocv/AppsActivity.java
+++ app/src/main/java/com/maufonseca/mauriciocv/AppsActivity.java
@@ -2,6 +2,8 @@ package com.maufonseca.mauriciocv;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class AppsActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class AppsActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_apps);
+    TextView titleView = findViewById(R.id.title_textview);
+    titleView.setText(getString(R.string.apps));
+  }
+
+  public void finishClicked(View v) {
+    finish();
   }
 }
--- app/src/main/java/com/maufonseca/mauriciocv/DevActivity.java
+++ app/src/main/java/com/maufonseca/mauriciocv/DevActivity.java
@@ -2,6 +2,8 @@ package com.maufonseca.mauriciocv;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class DevActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class DevActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_dev);
+    TextView titleView = findViewById(R.id.title_textview);
+    titleView.setText(getString(R.string.about));
+  }
+
+  public void finishClicked(View v) {
+    finish();
   }
 }
