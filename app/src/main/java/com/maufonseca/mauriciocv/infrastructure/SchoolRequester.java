2e31c11774db41a0968cfa9451d617ad010868c7
Maurício <maubfon@gmail.com>
2018-07-09 23:50:21 -0300

    Add detailed description on MBA formation

--- app/src/main/java/com/maufonseca/mauriciocv/infrastructure/SchoolRequester.java
+++ app/src/main/java/com/maufonseca/mauriciocv/infrastructure/SchoolRequester.java
@@ -12,13 +12,18 @@ public class SchoolRequester {
     Snippet mba = new Snippet("USP - Abr/2018 ao Presente",
         "Campus de Piracicaba/SP",
         "MBA em Gestão de Projetos",
-        "• Conclusão prevista para Out/2019");
+        "• Conclusão prevista para Out/2019\n"+
+            "• Fixação das boas práticas em gestão de projetos previstas no PMBOK\n"+
+            "• Gerenciamento de Integração, Escopo, Cronograma, Custos, Qualidade, Recursos Humanos, Riscos, Aquisições, Comunicações e Stakeholders\n"+
+            "• Contato com ferramentas de gerenciamento de projeto\n" +
+            "• Liderança e motivação como gerente de projetos\n"+
+            "• Gerenciamento com métodos ágeis e métodos híbridos");
     school.add(mba);
 
     Snippet engComp = new Snippet("USP - 2008 a 2017",
         "Campus de São Carlos/SP",
         "Graduação em Engenharia de Computação",
-        "• TCC publicado em 2016: “Teste de software em aplicativos móveis no ambiente empresarial: um relato de experiência”\n\n"+
+        "• TCC publicado em 2016: “Teste de software em aplicativos móveis no ambiente empresarial: um relato de experiência”\n"+
             "• Conclusão em Nov/2017");
     school.add(engComp);
     return school;
